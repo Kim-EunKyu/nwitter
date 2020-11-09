341d16cb522450b38d27e253fc4a6f52db29f0de
Kim-EunKyu <59270535+Kim-EunKyu@users.noreply.github.com>
2020-11-09 16:03:30 +0900

    #2 final submit

---
+++ src/routes/Profile.js
@@ -0,0 +1,68 @@
+import { authService, dbService } from "firebaseConfig";
+import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
+
+const Profile = ({ refreshUser, userObj }) => {
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+  const history = useHistory();
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+
+  const getMyNweets = async () => {
+    const nweets = await dbService
+      .collection("nweets")
+      .where("creatorId", "==", userObj.uid)
+      .orderBy("createdAt")
+      .get();
+    console.log(nweets.docs.map((doc) => doc.data()));
+  };
+  useEffect(() => {
+    getMyNweets();
+  }, []);
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({
+        displayName: newDisplayName,
+      });
+      refreshUser();
+    }
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={onSubmit} className="profileForm">
+        <input
+          onChange={onChange}
+          type="text"
+          autoFocus
+          placeholder="Display name"
+          value={newDisplayName}
+          className="formInput"
+        />
+        <input
+          type="submit"
+          value="Update Profile"
+          className="formBtn"
+          style={{
+            marginTop: 10,
+          }}
+        />
+      </form>
+      <span className="formBtn cancelBtn logOut" onClick={onLogOutClick}>
+        Log Out
+      </span>
+    </div>
+  );
+};
+
+export default Profile;
